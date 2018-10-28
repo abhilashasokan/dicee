68af572a83c4aec64010e19a09de4f60d9add40b
Abhilash <abhilash@heidelsofttechnologies.de>
2018-10-28 10:53:17 +0530

    added new values

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     var randomDiceIndex1:Int = 0
     var randomDiceIndex2:Int = 0
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -19,14 +20,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Roll pressed!")
+        updateDiceImages()
+    }
+    
+    //    Functions that update randon image
+    func updateDiceImages() {
         randomDiceIndex1 = Int(arc4random_uniform(5));
         randomDiceIndex2 = Int(arc4random_uniform(5));
-        print(randomDiceIndex1)
-        print(randomDiceIndex2)
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
